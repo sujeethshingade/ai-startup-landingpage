0a7354a2185ad5eee93cf5100c3a80f0b206e28f
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-12 13:34:47 +0530

    add hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,30 @@
+import { Button } from "@/components/Button";
+import starsBg from "@/assets/stars.png";
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]" style={{ backgroundImage: `url(${starsBg.src})`, }}>
+      <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
+      <div className="absolute w-64 h-64 md:w-96 md:h-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+      <div className="absolute w-[344px] h-[344px] md:w-[580px] md:h-[580px] rounded-full border opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute w-2 h-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute w-2 h-2 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute w-5 h-5 left-full border border-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+        </div>
+      </div>
+      <div className="absolute w-[444px] h-[444px] md:w-[780px] md:h-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></div>
+      <div className="absolute w-[544px] h-[544px] md:w-[980px] md:h-[980px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+        <div className="absolute w-2 h-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute w-2 h-2 left-full bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      </div>
+      <div className="container relative mt-16">
+        <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">AI SEO</h1>
+        <p className="text-lg md:text-xl text-white/70 mt-5 text-center max-w-xl mx-auto">Elevate your site's visibility effortlessly with AI, where smart technology meets user-friendly SEO tools.</p>
+        <div className="flex justify-center mt-5">
+          <Button>Join waitlist</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
