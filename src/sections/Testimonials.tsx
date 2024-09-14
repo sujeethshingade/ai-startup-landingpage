5ecff378a03c43e94c7dc075cecb064772a80885
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-14 19:45:14 +0530

    animate testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -41,9 +44,13 @@ export const Testimonials = () => {
         <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
           Our revolutionary AI SEO tools have transformed our clients&#39; strategies.
         </p>
-        <div className='overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-          <div className="flex gap-5">
-            {testimonials.map(testimonial => (
+        <div className='flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+          <motion.div 
+          initial={{translateX: '-50%'}}
+          animate={{translateX: '0'}}
+          transition={{duration: 30, repeat: Infinity, ease: 'linear'}}
+          className="flex flex-none pr-5 gap-5 -translate-x-1/2">
+            {[...testimonials, ...testimonials].map(testimonial => (
               <div key={testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
                 <div className="text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
                 <div className="flex items-center gap-3 mt-5">
@@ -57,7 +64,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
